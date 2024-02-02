8cad86b2f831b79307ce4fe831ac42ce6647e834
esyamamoto <erika.s.yamamoto@gmail.com>
2024-02-02 17:12:56 -0300

    nao tem test

--- app/backend/src/controller/user.controller.ts
+++ app/backend/src/controller/user.controller.ts
@@ -12,4 +12,8 @@ export default class UserController {
     const user = await this.newService.login(email, password);
     res.status(statusHTTPMap(user.status)).json(user.data);
 }
+  static async validateTokenRole(req: Request, res: Response) {
+    const { role } = res.locals.auth;
+    return res.status(200).json({ role });
+  }
 }
--- app/backend/src/routers/login.router.ts
+++ app/backend/src/routers/login.router.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
 import UserController from '../controller/user.controller';
 import ValidateLogin from '../middlewares/validateLogin';
+import ValidateToken from '../middlewares/validateToken';
 
 const routerInstance = Router();
 
@@ -8,6 +9,8 @@ const userController = new UserController();
 
 routerInstance.post('/', 
 ValidateLogin.validateLogin, (req: Request, res: Response) => userController.login(req, res));
+routerInstance.get('/role', ValidateToken.validateToken, (req: Request, res: Response) => 
+UserController.validateTokenRole(req, res));
 
 
 export default routerInstance;
