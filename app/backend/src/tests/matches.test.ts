febd6e3a980db6ca1f83dd654091da324cc13cb8
esyamamoto <erika.s.yamamoto@gmail.com>
2024-02-07 18:04:11 -0300

    79.16% testes

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -12,7 +12,9 @@ import TeamService from '../service/teams.service';
 import MatchController from '../controller/matches.controller';
 import { MatchesInterface } from '../Interfaces/macthes.interface';
 import { MatchModelInterface } from '../Interfaces/matches.interface.model';
-import ValidateMatch from '../middlewares/validateMatch';
+import ValidateToken from '../middlewares/validateToken';
+import { tokenOk } from '../tests/mocks/user.Mocks'
+import SequelizeMatches from '../database/models/Sequelize.Matches.Model';
 
 
 chai.use(chaiHttp);
@@ -43,17 +45,6 @@ describe('Matches', function () {
     expect(body).to.be.deep.equal({ message: 'Token must be a valid token' })
   });
 
-  it('Verifica se retorna um error, caso seja enviado um token inválido', async function () {
-
-    sinon.stub(jwt, 'verify').throws(new Error('Token must be a valid token'))
-    const { status, body } = await chai.request(app).post("/matches")
-    .set({'Authorization': `Bea any-token`})
-    .send(matchesMock.NewMatches)
-
-    expect(status).to.be.equal(401)
-    expect(body).to.be.deep.equal({ message: 'Token must be a valid token' })
-  });
-
   afterEach(sinon.restore);
 })
 
@@ -67,53 +58,54 @@ describe('MatchService', function () {
     afterEach(function () {
       sinon.restore();
     });
+    it('Verifica se retorna todas as partidas com requisição GET para /matches', async function () {
+      sinon.stub(MatchModel.prototype, 'allMatches').resolves(matchesMock.matchesOk as any);
   
-    it('Deve retornar todos os jogos em andamento', async function () {
+      const {status, body} = await chai.request(app).get("/matches")
+  
+      expect(status).to.be.equal(200)
+      expect(body).to.be.deep.equal(matchesMock.matchesOk)
+    });
+    it('Verifica se retorna todos os jogos em andamento', async function () {
         sinon.stub(MatchModel.prototype, 'allMatches').resolves(matchesMock.matchesOk);
         const result = await matchService.allMatches(true);
         expect(result.status).to.equal('SUCCESSFUL');
         expect(result.data).to.deep.equal(matchesMock.matchesOk);
     });
   
-    it('Deve retornar todos os jogos finalizados', async function () {
+    it('Verifica se retorna todos os jogos finalizados', async function () {
       sinon.stub(MatchModel.prototype, 'allMatches').resolves(matchesMock.finishMatches);
       const result = await matchService.allMatches(false);
       expect(result.status).to.equal('SUCCESSFUL');
       expect(result.data).to.deep.equal(matchesMock.finishMatches);
     });
 
+
+
+
+    it('Verifica se é possível criar uma partida com POST para /matches', async function () {
+      sinon.stub(jwt, 'verify').resolves({ role: "admin"});
+      const newTeam = SequelizeMatches.build(matchesMock.NewTeam);
+      sinon.stub(SequelizeMatches, 'create').resolves(newTeam);
+  
+      const { status, body } = await chai.request(app).post("/matches")
+      .set({'Authorization': `Bearer ${tokenOk}`})
+      .send(matchesMock.NewMatches)
+  
+      expect(status).to.be.equal(201)
+      expect(body).to.be.deep.equal(matchesMock.NewTeam)
+    })
+  /*
     it('Deve atualizar uma partida ao acessar /updatedMatches/:id', async function () {
       sinon.stub(MatchService.prototype, 'updatedMatches').resolves({ status: 'SUCCESSFUL', data: 'Match updated' });
       const { status, body } = await chai.request(app).put("/updatedMatches/1").send({ homeTeamGoals: 2, awayTeamGoals: 1 })
       expect(status).to.be.equal(200);
       expect(body).to.be.deep.equal('Match updated');
     });
-    /*
     it('Deve criar uma nova partida ao acessar /createdMatches', async function () {
       sinon.stub(MatchService.prototype, 'createdMatch').resolves({ status: 'CREATED' , data: matchesMock.NewTeam});
       const { status, body } = await chai.request(app).post("/createdMatches").send(matchesMock.NewMatches);
       expect(status).to.be.equal(201);
       expect(body).to.be.deep.equal(matchesMock.NewTeam);
-    });
-
-    it('Deve retornar status 422 para equipes de mesmo ID', function () {
-      const req = { body: { homeTeamId: 1, awayTeamId: 1 } } as Request;
-      const res = { status: sinon.stub().returnsThis(), json: sinon.stub() } as unknown as Response;
-      const next = sinon.stub();
-  
-      ValidateMatch.validateMatch(req, res, next);
-  
-      expect(res.status.calledWith(422)).to.be.true;
-      expect(res.json.calledWith({ message: 'It is not possible to create a match with two equal teams' })).to.be.true;
-    });
-  
-    it('Deve chamar next() para equipes diferentes', function () {
-      const req = { body: { homeTeamId: 1, awayTeamId: 2 } } as Request;
-      const res = {} as Response;
-      const next = sinon.stub();
-  
-      ValidateMatch.validateMatch(req, res, next);
-  
-      expect(next.called).to.be.true;
     }); */
   });
